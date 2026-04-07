4794695c478df348d13ccb63fa248cd9526816b6
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-04-07 16:14:37 +0200

    Replaced Navbar.jsx and Navbar.css with clean fixed versions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
+import "../styles/navbar.css";
 
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -12,16 +13,13 @@ export default function Navbar() {
         <>
             <nav className="navbar">
                 <Link to="/" className="navbar-logo">
-                    <img src="/logo.png" alt="Logo" className="logo-img" />
+                    <img src="/logo.png" alt="Logo" />
                     <span className="navbar-title">PlusMarket Giuntelli</span>
                 </Link>
 
-                {/* 🔥 Carrello SEMPRE visibile anche su mobile */}
-                <Link to="/cart" className="cart-link mobile-cart">
-                    <div className="cart-info">
-                        <span className="cart-count">🛒 {cartCount}</span>
-                        <span className="cart-total">€{total.toFixed(2)}</span>
-                    </div>
+                <Link to="/cart" className="cart-link">
+                    <span className="cart-badge">{cartCount}</span>
+                    🛒 €{total.toFixed(2)}
                 </Link>
 
                 <div
