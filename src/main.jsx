41e00b828e848e5f08c16469092ca05c6e4ad0d9
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-04-24 15:13:20 +0200

    Fix struttura App + ripristino Navbar mobile + carrello visibile

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import "./styles/theme.css";
 import { registerSW } from "virtual:pwa-register";
-
-// PAGINE
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";   // ⭐ CORRETTO
-import Checkout from "./pages/Checkout";
-import Grazie from "./pages/Grazie";
-import ChiSiamo from "./pages/ChiSiamo";
-import Conditions from "./pages/Conditions";     // ⭐ NUOVA PAGINA
 
 registerSW({
   immediate: true,
@@ -20,22 +12,7 @@ registerSW({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        {/* ⭐ PAGINA PRODOTTI */}
-        <Route path="/prodotti" element={<ProductList />} />
-
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/grazie" element={<Grazie />} />
-        <Route path="/chi-siamo" element={<ChiSiamo />} />
-
-        {/* ⭐ PAGINA CONDIZIONI DI VENDITA */}
-        <Route path="/condizioni" element={<Conditions />} />
-
-        {/* fallback */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </CartProvider>
 );
